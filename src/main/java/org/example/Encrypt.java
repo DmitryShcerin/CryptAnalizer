1b3451db0c133c66b4fdb4410edfe9d2e60ccf18
DmitryShcerin <shcherin95@mail.ru>
2025-05-29 19:12:12 +0300

    Added Encrypt class and encryption/decryption logic

---
+++ src/main/java/org/example/Encrypt.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public class Encrypt {
+    private static final String ALPHABET = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя" +
+            "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ" +
+            ".,\"’:-!? \n\t";
+
+
+    public static String encrypt(String data, int key) {
+        StringBuilder result = new StringBuilder();
+
+        for (char ch : data.toCharArray()) {
+            int index = ALPHABET.indexOf(ch);
+            if (index != -1) {
+                int shiftedIndex = (index + key) % ALPHABET.length();
+                if (shiftedIndex < 0){
+                    shiftedIndex+=ALPHABET.length();
+                }
+                result.append(ALPHABET.charAt(shiftedIndex));
+            }
+            else {
+                result.append(ch);
+            }
+        }
+        return result.toString();
+    }
+
+    public static String decrypt(String data, int key){
+        return encrypt(data,-key);
+    }
+}
